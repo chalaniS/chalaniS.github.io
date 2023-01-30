2ca4461e544902600c62c3cfc33ef989068da523
Chalani Saumya Rajapaksha <chalasaumya@gmail.com>
2023-01-30 17:31:12 +0530

    demo done

--- js/script.js
+++ js/script.js
@@ -12,6 +12,27 @@ $(document).ready(function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
+        if( $(window).scrollTop()>0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+
+
+    });
+
+    // smooth srolling
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+
+        500,
+        'linear'
+        
+        );
 
     });
 
