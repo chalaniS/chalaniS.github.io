b99867c1a9839c8f1f190dc31807fcf4f2e8e6d6
Chalani Saumya Rajapaksha <chalasaumya@gmail.com>
2023-02-03 22:12:30 +0530

    Successfully Completed

--- js/script.js
+++ js/script.js
@@ -1,37 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#menu').click(function(){
+    $('#menu').click(function () {
 
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
 
     });
 
-    $(window).on('scroll load', function(){
-        
+    $(window).on('scroll load', function () {
+
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
-        if( $(window).scrollTop()>0){
+        if ($(window).scrollTop() > 0) {
             $('.top').show();
-        }else{
+        } else {
             $('.top').hide();
         }
-
 
     });
 
     // smooth srolling
-    $('a[href*="#"]').on('click', function(e){
+    $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
 
         $('html, body').animate({
-            scrollTop : $($(this).attr('href')).offset().top,
+            scrollTop: $($(this).attr('href')).offset().top,
         },
 
-        500,
-        'linear'
-        
+            500,
+            'linear'
+
         );
 
     });
